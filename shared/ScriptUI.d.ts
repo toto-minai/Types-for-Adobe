d8915a7e559ef1f4a8706bc96552a795a40822c4
Toto Minai <bufhdy@hotmail.com>
2021-10-30 15:00:21 +0900

    Extend text for DropDownList

--- shared/ScriptUI.d.ts
+++ shared/ScriptUI.d.ts
@@ -1243,6 +1243,11 @@ declare class ListBox extends _Control {
  */
 declare class DropDownList extends _Control {
   /**
+   * Label of the element
+   */
+  text: string
+
+  /**
    * True if this element is active.
    * An active control is the one with keyboard focus—that is, the one that accepts keystrokes, or in the case of a Button, is selected when the user types Return or Enter in Windows, or the space bar in Mac OS.
    */
